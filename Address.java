0928dd5ef10d214823fd336350538bad7782ca86
yatiiazmi <118959268+yatiiazmi@users.noreply.github.com>
2023-03-06 00:30:45 +0800

    Create Address.java

---
+++ Address.java
@@ -0,0 +1,38 @@
+public class Address {
+    private String street;
+    private String district;
+    private String postcode;
+    private String state;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
